0fab3f0cb111b53fcdfcde935d5d71bd4d06f9f2
Abdo Sharaf <bodyasharaf8@gmail.com>
2023-06-26 14:49:17 +0300

    Bind data into the details screen

--- app/src/main/java/com/abdosharaf/asteroidradar/detail/DetailFragment.kt
+++ app/src/main/java/com/abdosharaf/asteroidradar/detail/DetailFragment.kt
@@ -1,19 +1,37 @@
 package com.abdosharaf.asteroidradar.detail
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import com.abdosharaf.asteroidradar.R
 import com.abdosharaf.asteroidradar.databinding.FragmentDetailBinding
 
 class DetailFragment : Fragment() {
 
     private lateinit var binding: FragmentDetailBinding
+    private val args: DetailFragmentArgs by navArgs()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentDetailBinding.inflate(inflater, container, false)
 
+        binding.lifecycleOwner = this
+        binding.asteroid = args.selectedAsteroid
+
+        binding.helpButton.setOnClickListener {
+            displayDialog()
+        }
+
         return binding.root
+    }
+
+    private fun displayDialog() {
+        val builder = AlertDialog.Builder(requireActivity())
+            .setMessage(getString(R.string.astronomica_unit_explanation))
+            .setPositiveButton(android.R.string.ok, null)
+        builder.create().show()
     }
 }
